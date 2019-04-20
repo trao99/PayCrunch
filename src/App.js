1cc8008c38861db5e631f02d42492f0113efeb60
trao99 <trishankrao@gmail.com>
2019-04-20 15:03:22 -0400

    Removed firebase, added mongodb

--- src/App.js
+++ src/App.js
@@ -1,32 +1,10 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
 
 class App extends Component {
-
-  constructor(){
-    super();
-    this.state = {
-        budget: 10
-    };
-  }
-
-  componentDidMount() {
-
-    const rootRef = firebase.database().ref().child('react');
-    const budgetRef = rootRef.child('budget');
-    budgetRef.on('value',snap => {
-        this.setState({
-            budget: snap.val{}
-        });
-    });
-  }
-
   render() {
     return (
-      <div className = "App">
-        <h1>{this.state.budget}</h1>
-      </div>
+      <div> back-end api ready to use </div>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import * as firebase from 'firebase';
-
-var config = {
-    
-  };
-  firebase.initializeApp(config);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
