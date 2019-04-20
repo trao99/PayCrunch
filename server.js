ed205cc0d8a53c1ff08131f245af6163a5e686bb
trao99 <trishankrao@gmail.com>
2019-04-20 16:53:44 -0400

    connects to mongodb succesfully

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-onst mongoose = require(“mongoose”);
-const express = require(“express”);
-var cors = require(‘cors’);
-const bodyParser = require(“body-parser”);
-const logger = require(“morgan”);
-const Data = require(“./data”);
+const mongoose = require("mongoose");
+const express = require("express");
+var cors = require('cors');
+const bodyParser = require("body-parser");
+const logger = require("morgan");
+const Data = require("./data");
 
 const API_PORT = 3001;
 const app = express();
