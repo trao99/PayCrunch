c1f2ce70f0783eed9af56cb9d7459e25c74f2de7
Yazin Farook <yfarook@purdue.edu>
2019-04-25 20:24:33 -0400

    global username

--- finalDeploy/routes/users.js
+++ finalDeploy/routes/users.js
@@ -79,6 +79,7 @@ router.post('/register', (req, res) => {
 });
 
 router.post('/login', (req, res, next) => {
+    username = req.body.email;
   passport.authenticate('local', {
     //successRedirect: '/dashboard',
     successRedirect: '/index.html',
