e1bfe50cf068c1416cdf6fbb27ecaa04c7842202
trao99 <trishankrao@gmail.com>
2019-04-26 11:26:03 -0400

    Frontend modifications

--- public/js/demo/chart-area-demo.js
+++ public/js/demo/chart-area-demo.js
@@ -7,7 +7,7 @@ var ctx = document.getElementById("myAreaChart");
 var myLineChart = new Chart(ctx, {
   type: 'line',
   data: {
-    labels: ["Mar 1", "Mar 2", "Mar 3", "Mar 4", "Mar 5", "Mar 6", "Mar 7", "Mar 8", "Mar 9", "Mar 10", "Mar 11", "Mar 12", "Mar 13"],
+    labels: ["Apr 14", "Apr 15", "Apr 16", "Apr 17", "Apr 18", "Apr 19", "Apr 20", "Apr 21", "Apr 22", "Apr 23", "Apr 24", "Apr 25", "Apr 26"],
     datasets: [{
       label: "Sessions",
       lineTension: 0.3,
